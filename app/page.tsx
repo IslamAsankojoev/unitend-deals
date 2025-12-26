c762a70f51622dd8108745243b7580d9f7891681
akolya <115164991+akolya@users.noreply.github.com>
2025-12-27 07:45:58 +0900

    solved git

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,13 @@ export default function Home() {
   return (
     <>
       {/* pagination section */}
-
       {/* faq section */}
-
       {/* Shop section */}
-
       {/* deals section */}
-
       {/* shop categories section */}
-
       {/* brands slider */}
-
       {/* footer */}
+      solved trouble with git
     </>
   )
 }
